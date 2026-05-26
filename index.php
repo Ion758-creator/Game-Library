c1219d7c03c498825b1af2fa6d62878ab6df06d7
Ion758-creator <ionsaptefrati87@gmail.com>
2026-05-26 08:35:16 +0300

    Mesaj in consola

--- index.php
+++ index.php
@@ -3,19 +3,28 @@ $games = ["GTA V", "Minecraft", "Valorant", "FIFA 24"];
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="ro">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Library</title>
 </head>
+
 <body>
 
-<h1>Game Library</h1>
+    <h1>Game Library</h1>
 
-<ul>
-    <?php foreach ($games as $game): ?>
-        <li><?php echo $game; ?></li>
-    <?php endforeach; ?>
-</ul>
+    <ul>
+        <?php foreach ($games as $game): ?>
+            <li><?php echo $game; ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+    <script>
+        console.log("Pagina principala functioneaza!");
+    </script>
 
 </body>
+
 </html>
---
+++ php/auth.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Salut! PHP merge!</h1>";
+?>
+
+<script>
+console.log("Salut! PHP merge!");
+</script>
